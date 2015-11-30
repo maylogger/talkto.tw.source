56dbda4ca4c37e20459815314b56e29746f42bcd
TouNeko <touneko@gmail.com>
2015-11-30 23:10:43 +0800

    套正確的 class 名稱

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -4,7 +4,6 @@ var app = angular.module('project', ['angularUtils.directives.dirPagination']);
 
 app.controller('listController', function($scope,$http,$timeout) {
 
-  $scope.sort = 'percentage';
   $scope.currentPage = 1;
   $scope.pageSize = 5;
   // data 陣列名稱宣告
@@ -12,15 +11,15 @@ app.controller('listController', function($scope,$http,$timeout) {
   $scope.books = [];
 
   // import json-a
-  $http.get('/json/01.json').success(function(data) {
+  $http.get('/json/talk01-01.json').success(function(data) {
     $scope.tallys = data;
-    $('.point-of-view-list').delay(500).fadeIn('slow');
+    $('.polis-list').delay(500).fadeIn('slow');
   });
 
   // import json-b
-  $http.get('/json/02.json').success(function(data) {
+  $http.get('/json/talk01-02.json').success(function(data) {
     $scope.books = data;
-    $('.point-of-view-list').delay(500).fadeIn('slow');
+    $('.polis-list').delay(500).fadeIn('slow');
   });
 
 });
