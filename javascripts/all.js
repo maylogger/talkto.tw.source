1d30022349c4b75f82c155df28522db13ec34355
TouNeko <touneko@gmail.com>
2015-12-07 22:05:55 +0800

    update

--- javascripts/all.js
+++ javascripts/all.js
@@ -24,3 +24,33 @@ if ($('.program-slider').length != 0) {
     ]
   });
 }
+
+if ($('#polis-link').length != 0) {
+  $('#polis-link').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+    var targetScrollTop = $target.offset().top;
+
+    $('html, body').stop().animate({
+        'scrollTop': targetScrollTop
+    }, 500, 'swing');
+  });
+}
+
+if ( $('.animation').length != 0 ) {
+  function section_animation() {
+    var animationElements = $('.animation')
+    for (var i = 0; i < animationElements.length; i++) {
+      new Waypoint({
+        element: animationElements[i],
+        handler: function(direction) {
+          $(this.element).addClass('is-active')
+        },
+        offset: '100%'
+      })
+    }
+  }
+  section_animation();
+}
