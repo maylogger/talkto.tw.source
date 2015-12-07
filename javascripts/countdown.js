bf3420b62caff2e560ffaf118f2b9cab600690ff
TouNeko <touneko@gmail.com>
2015-12-07 21:43:36 +0800

    bug fix

--- javascripts/countdown.js
+++ javascripts/countdown.js
@@ -1,5 +1,5 @@
 var openTime = moment.tz("2015-12-04 21:00", "Asia/Taipei");
-var closeTime = moment.tz("2015-12-04 21:30", "Asia/Taipei");
+var closeTime = moment.tz("2015-12-04 22:00", "Asia/Taipei");
 
 $('.countdown').countdown(openTime.toDate()).on('update.countdown', function(event) {
   $('.live-countdown').addClass('counting');
@@ -18,6 +18,11 @@ $('.countdown').countdown(openTime.toDate()).on('update.countdown', function(eve
       + '<div class="side-content chat"><div class="chatroom"><iframe src="https://livehouse.in/embed/channel/talktotaiwan/chatroom/dark" frameborder="0" allowfullscreen=""></iframe></div></div>'
     );
   }
+}).on('finish.countdown', function(event) {
+    $('.livestream').html(''
+      + '<div class="main-content video"><div class="stream"><iframe src="https://livehouse.in/embed/channel/talktotaiwan/video" frameborder="0" allowfullscreen=""></iframe></div></div>'
+      + '<div class="side-content chat"><div class="chatroom"><iframe src="https://livehouse.in/embed/channel/talktotaiwan/chatroom/dark" frameborder="0" allowfullscreen=""></iframe></div></div>'
+    );
 });
 
 $('.now-program .link').countdown(closeTime.toDate()).on('update.countdown', function(event) {
