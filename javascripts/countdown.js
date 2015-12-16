6228e307ab6b605f4e67271ba0bac46f641f8550
TouNeko <touneko@gmail.com>
2015-12-16 15:04:27 +0800

    update

--- javascripts/countdown.js
+++ javascripts/countdown.js
@@ -1,5 +1,5 @@
-var openTime = moment.tz("2015-12-04 21:00", "Asia/Taipei");
-var closeTime = moment.tz("2015-12-04 22:00", "Asia/Taipei");
+var openTime = moment.tz("2015-12-16 21:00", "Asia/Taipei");
+var closeTime = moment.tz("2015-12-16 22:00", "Asia/Taipei");
 
 $('.countdown').countdown(openTime.toDate()).on('update.countdown', function(event) {
   $('.live-countdown').addClass('counting');
@@ -12,7 +12,7 @@ $('.countdown').countdown(openTime.toDate()).on('update.countdown', function(eve
     + '<div class="colon">：</div>'
     + '<div class="item"><div class="number">%S</div><div class="unit">SECS</div></div>'
   ));
-  if (event.offset.hours < 1) {
+  if (event.offset.days < 1 && event.offset.hours < 1) {
     $('.livestream').html(''
       + '<div class="main-content video"><div class="stream"><iframe src="https://livehouse.in/embed/channel/talktotaiwan/video" frameborder="0" allowfullscreen=""></iframe></div></div>'
       + '<div class="side-content chat"><div class="chatroom"><iframe src="https://livehouse.in/embed/channel/talktotaiwan/chatroom/dark" frameborder="0" allowfullscreen=""></iframe></div></div>'
@@ -26,7 +26,7 @@ $('.countdown').countdown(openTime.toDate()).on('update.countdown', function(eve
 });
 
 $('.now-program .link').countdown(closeTime.toDate()).on('update.countdown', function(event) {
-  if (event.offset.hours < 1) {
+  if (event.offset.days < 1 && event.offset.hours < 1) {
     $(this).html('觀看直播 Live →');
   }
 }).on('finish.countdown', function(event) {
